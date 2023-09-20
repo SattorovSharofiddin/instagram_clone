ef416354c32b5693c832d27b4cf56025a33b40fd
sattorovsharofiddin <cyberpancher10@gmail.com>
2023-09-20 18:01:07 +0500

    add users, shared

--- root/settings.py
+++ root/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
 
+    'users',
+    'shared'
+
 ]
 
 MIDDLEWARE = [
@@ -98,3 +101,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ shared/models.py
@@ -0,0 +1,12 @@
+import uuid
+
+from django.db.models import DateTimeField, UUIDField, Model
+
+
+class BaseModel(Model):
+    id = UUIDField(unique=True, default=uuid.uuid4, editable=False, primary_key=True)
+    created_time = DateTimeField(auto_now_add=True)
+    updated_time = DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
